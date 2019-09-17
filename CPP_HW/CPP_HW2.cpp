a36df5805bdbbb48fd58077ec1729970d0721369
Tu Tong <ttwoosta@gmail.com>
2019-09-17 10:45:55 -0400

    Done with searching and listing books.

--- CPP_HW/CPP_HW2.cpp
+++ CPP_HW/CPP_HW2.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <cassert>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -46,6 +47,11 @@ public:
 	// access the first node of the list
 	bookType* firstBook();
 
+	// search a book by book bumber
+	bookType* searchBook(int bookNum);
+
+	// remove a book from linked list
+	bookType* deleteBook(int bookNum);
 
 protected:
 	int count; // variable to store the number of element
@@ -72,11 +78,59 @@ int main()
 	bookList->insertAt(new bookType(8, "DATABASE IMPLEMENTATION", "ADAMS", "READING MATERIALS INC.", 642, 24.50), 2);
 	bookList->insertAt(new bookType(9, "E-BUSINESS THE EASY WAY", "JONES", "PUBLISH OUR WAY", 328, 37.99), 2);
 
-	bookType *currBook = bookList->firstBook();
-	while (currBook != NULL)
+	int bookNum = 1;
+	bookType* aBook;
+
+	cout << "*************************************************" << endl;
+	cout << "Welcome to my book store" << endl;
+	cout << "*************************************************" << endl;
+
+	while (bookNum != 0)
 	{
-		cout << currBook->desc() << endl;
-		currBook = currBook->link;
+		cout << "The database currently contains " << bookList->length() << " books." << endl;
+		cout << "Press number 0 to exit, press 99 to print." << endl;
+		cout << "----------------------------------------------" << endl;
+		
+		cout << "Enter any number from 1 - 10 to search for a book: ";
+		cin >> bookNum;
+
+		// Restore the input stream
+		cin.clear();
+
+		if (bookNum > 0 && bookNum < 11) {
+			aBook = bookList->searchBook(bookNum);
+			if (aBook == NULL) {
+				cout << "The book " << bookNum << " has been deleted" << endl;
+			}
+			else {
+				cout << "Found 1 book" << endl;
+				cout << setfill('.') << left << setw(35) << "Book number: "
+					<< right << " " << aBook->bookNumber << endl;
+				cout << setfill('.') << left << setw(35) << "Title: "
+					<< right << " " << aBook->title << endl;
+				cout << setfill('.') << left << setw(35) << "Author last name: "
+					<< right << " " << aBook->authorLastName << endl;
+				cout << setfill('.') << left << setw(35) << "Publisher: "
+					<< right << " " << aBook->publisher << endl;
+				cout << setfill('.') << left << setw(35) << "Number of pages: "
+					<< right << " " << to_string(aBook->numberOfPages) << endl;
+				cout << setfill('.') << left << setw(35) << "Price: " << setfill(' ')
+					<< right << " $" << setw(8) << aBook->price << endl;
+			}
+		}
+		else if (bookNum == 99) {
+			bookType *currBook = bookList->firstBook();
+			while (currBook != NULL)
+			{
+				cout << currBook->desc() << endl;
+				currBook = currBook->link;
+			}
+		}
+		else {
+			cout << "No book found" << endl;
+		}
+
+		cout << "------------------------------------------------------------------\n\n";
 	}
 
 	// remove list of books from memory
@@ -165,4 +219,34 @@ void linkedListBook::insertAt(bookType* newBook, int position) {
 	pre->link = newBook;
 	newBook->link = curr;
 	count++;
+}
+
+bookType* linkedListBook::searchBook(int bookNum) {
+	bookType* currBook = first;
+	while (currBook != NULL)
+	{
+		if (bookNum == currBook->bookNumber)
+			return currBook;
+		currBook = currBook->link;
+	}
+	return NULL;
+}
+
+bookType* linkedListBook::deleteBook(int bookNum) {
+	bookType* currBook = first;
+	bookType* pre = currBook;
+
+	while (currBook != NULL)
+	{
+		if (bookNum == currBook->bookNumber) 
+		{
+			pre->link = currBook->link;
+			count--;
+			return currBook;
+		}
+		pre = currBook;
+		currBook = currBook->link;
+	}
+
+	return NULL;
 }
