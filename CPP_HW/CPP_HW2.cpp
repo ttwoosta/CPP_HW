305220bb33a811b494420aa1d1c731765b2325db
Tu Tong <ttwoosta@gmail.com>
2019-09-16 21:30:13 -0400

    Added linked list that store books

--- CPP_HW/CPP_HW2.cpp
+++ CPP_HW/CPP_HW2.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+#include <cassert>
+#include <string>
 
 using namespace std;
 
-struct Book
+struct bookType
 {
 	int bookNumber;
 	string title;
@@ -14,29 +16,153 @@ struct Book
 	int numberOfPages;
 	double price;
 
-	Book* link;
+	// contructor
+	bookType(int, string, string, string, int, double);
+	string desc();
+
+	bookType* link;
+};
+
+class linkedListBook
+{
+public:
+	// default contructor
+	// initialize the list to an empty state
+	linkedListBook();
+
+	// destructor
+	// Delete all nodes from the list
+	~linkedListBook();
+
+	// the length of the linked list
+	int length();
+
+	// insert node on the top
+	void insertFirst(bookType* newBook);
+
+	// insert node at position
+	void insertAt(bookType* newBook, int position);
+
+	// access the first node of the list
+	bookType* firstBook();
+
+
+protected:
+	int count; // variable to store the number of element
+
+	bookType* first; // pointer to the first node of the list
+	bookType* last;  // pointer to the last node of the list
+private:
+
 };
 
 int main()
 {
-	Book *head = new Book();
-	head->bookNumber = 12323132;
-	head->title = "Linked list";
-	head->authorLastName = "Tong";
-	head->publisher = "Tu Tong";
-	head->numberOfPages = 1000;
-	head->price = 300;
+	linkedListBook *bookList = new linkedListBook();
+	bookList->insertFirst(new bookType(1, "HOW TO GET FASTER PIZZA", "SMITH", "READING MATERIALS INC.", 534, 17.0));
+	bookList->insertFirst(new bookType(2, "THE WOK WAY TO COOK", "KZOCHSKY", "REED-N-RITE", 339, 23.15));
+	bookList->insertFirst(new bookType(3, "REVENGE OF MICKEY", "JONES", "PRINTING IS US", 740, 14.0));
+	bookList->insertFirst(new bookType(4, "BODYBUILD IN 10 MINUTES A DAY", "PORTER", "READING MATERIALS INC.", 532, 18.50));
+	bookList->insertFirst(new bookType(5, "HANDCRANKED COMPUTERS", "WHITE", "AMERICAN PUBLISHING", 457, 67.99));
+	bookList->insertFirst(new bookType(6, "PAINLESS CHILD-REARING", "BAKER", "AMERICAN PUBLISHING", 864, 48.98));
+	bookList->insertFirst(new bookType(7, "COOKING WITH MUSHROOMS", "ROBINSON", "READING MATERIALS INC.", 231, 12.50));
+	bookList->insertFirst(new bookType(10, "HOLY GRAIL OF ORACLE", "AUSTIN", "AMERICAN PUBLISHING", 789, 16.25));
 
-	cout << "My book " + head->title << "\n";
+	// insert a book at position 2
+	bookList->insertAt(new bookType(8, "DATABASE IMPLEMENTATION", "ADAMS", "READING MATERIALS INC.", 642, 24.50), 2);
+	bookList->insertAt(new bookType(9, "E-BUSINESS THE EASY WAY", "JONES", "PUBLISH OUR WAY", 328, 37.99), 2);
+
+	bookType *currBook = bookList->firstBook();
+	while (currBook != NULL)
+	{
+		cout << currBook->desc() << endl;
+		currBook = currBook->link;
+	}
+
+	// remove list of books from memory
+	delete bookList;
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bookType::bookType(int bookNum, string bookTitle, string authorLName, string pub, int pages, double bookPrice) {
+	bookNumber = bookNum;
+	title = bookTitle;
+	authorLastName = authorLName;
+	publisher = pub;
+	numberOfPages = pages;
+	price = bookPrice;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+string bookType::desc() {
+	// to_string method
+	// https://stackoverflow.com/questions/5590381/easiest-way-to-convert-int-to-string-in-c
+	return "Book #" + to_string(bookNumber) + ": " + title + 
+		" (" + to_string(numberOfPages) + " pages)" +
+		", by " + authorLastName + ", publish by " + publisher +
+		", price $" + to_string(price);
+}
+
+linkedListBook::linkedListBook()
+{
+	first = NULL;
+	last = NULL;
+	count = 0;
+}
+
+linkedListBook::~linkedListBook()
+{
+	bookType* temp;
+	while (first != NULL)
+	{
+		temp = first;
+		first = first->link; // advance first node to next node
+		delete temp;
+	}
+	last = NULL; // initialize the last node to NULL
+	count = 0;
+}
+
+int linkedListBook::length() 
+{
+	return count;
+}
+
+void linkedListBook::insertFirst(bookType* newBook) {
+	// make sure new book never be empty
+	assert(newBook != NULL);
+
+	newBook->link = first;
+	first = newBook;
+	count++;  // increment count
+
+	// if the list was empty, new book is also
+	// the last node of the list
+	if (last == NULL)
+		last = newBook;
+}
+
+bookType* linkedListBook::firstBook() {
+	return first;
+}
+
+void linkedListBook::insertAt(bookType* newBook, int position) {
+	// make sure new book never be empty
+	assert(newBook != NULL);
+
+	if (position > count)
+		return;
+
+	bookType* curr = first;
+	bookType* pre = curr;
+		
+	for (int i = 1; i < position; i++)
+	{
+		pre = curr;
+		curr = curr->link;
+	}
+
+	pre->link = newBook;
+	newBook->link = curr;
+	count++;
+}
