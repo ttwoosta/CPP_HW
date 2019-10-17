ce48f82ab6fafc8e5217c4d42ac5b4bee1a02ced
Tu Tong <ttwoosta@gmail.com>
2019-10-16 20:07:54 -0400

    Added comments.

--- CW11/CW11.cpp
+++ CW11/CW11.cpp
@@ -1,6 +1,4 @@
-// Inheritance example 
-
-
+// Inheritance Class work 11
 #include <iostream> 
 using namespace std;
 
@@ -9,6 +7,7 @@ class bankAccount
 public:
 	double balance;
 
+	// a virtual function that allow subclass override
 	double virtual new_balance() {
 		return 0;
 	}
@@ -18,23 +17,25 @@ class CheckingAccount : public bankAccount
 {
 public:
 	int number_of_checkbooks;
-public:
+	
+	// Calculate the total cost of the checkbooks
 	double total_cost() {
 		return number_of_checkbooks * 14.99;
 	}
 
+	// Calculate the new balance of Checking account
 	double new_balance() {
 		return balance - (number_of_checkbooks * 14.99);
 	}
 };
-
 
 class SavingAccount : public bankAccount
 {
 public:
 	double interest_rate;
 	int number_of_saving_years;
-public:
+
+	// calculate the new balance of Saving account
 	double new_balance() {
 		double new_bl = balance;
 		for (int i = 0; i < number_of_saving_years; i++)
@@ -47,13 +48,16 @@ public:
 
 int main()
 {
-
+	// declare 2 account Saving and Checking
 	CheckingAccount account1;
 	SavingAccount account2;
 	
+	// set the inital balance and number of checkbooks for Checking account
 	account1.balance = 1500.00;
 	account1.number_of_checkbooks = 2;
 
+	// set the inital balance, interest_rate, and number of saving years
+	// for the Saving Account
 	account2.balance = 400.00;
 	account2.interest_rate = 0.057;
 	account2.number_of_saving_years = 2;
@@ -66,8 +70,6 @@ int main()
 	cout << "balance of account2 is $" << account2.balance << endl;
 	cout << "number of interest rate is " << account2.interest_rate * 100 << "%" << endl;
 	cout << "new balance of account2 is $" << account2.new_balance() << endl;
-
-	//cout << "balance of account3 is " << account3.balance << endl;
 
 	return 0;
 }
