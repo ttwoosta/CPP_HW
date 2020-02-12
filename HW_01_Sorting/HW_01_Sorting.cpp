33e60e3a16c16ec67a49f8305d114a18f27c3c5d
Tu Tong <ttwoosta@gmail.com>
2020-02-11 22:07:39 -0500

    Implemented Median Sort (not fully working).

--- HW_01_Sorting/HW_01_Sorting.cpp
+++ HW_01_Sorting/HW_01_Sorting.cpp
@@ -1,10 +1,10 @@
 /*
-In this assignment you will be implementing the sorting algorithms 
-that we have discussed so far to compare how their actual run times 
+In this assignment you will be implementing the sorting algorithms
+that we have discussed so far to compare how their actual run times
 compare to their time complexities.
 
-1.) In c++, implement a 'List' class that represents a list of numbers 
-of a given size and create a function that is able to randomize your 
+1.) In c++, implement a 'List' class that represents a list of numbers
+of a given size and create a function that is able to randomize your
 list of numbers
 
 2.) Implement the following methods to sort your List class:
@@ -13,19 +13,19 @@ list of numbers
 - quick sort
 - median sort
 
-3.) Run each of these methods and record the run time of each at 
-List lengths of 10, 100, 1000, 10000, and 100000. Run each of 
-these 10 times and record the average of each run time - write 
+3.) Run each of these methods and record the run time of each at
+List lengths of 10, 100, 1000, 10000, and 100000. Run each of
+these 10 times and record the average of each run time - write
 these out to a csv file
 
-4.) In excel (or similar tool) graph the average run time for 
-each sorting method on the same plot to compare how each performs 
+4.) In excel (or similar tool) graph the average run time for
+each sorting method on the same plot to compare how each performs
 on each List size.
 
-5.) Repeat steps 3-4 when you attempt to sort an already-sorted list 
+5.) Repeat steps 3-4 when you attempt to sort an already-sorted list
 of numbers and record the findings
 
-6.) Repeat steps 3-4 when you attempt to sort a reverse-sorted list 
+6.) Repeat steps 3-4 when you attempt to sort a reverse-sorted list
 of numbers and record the findings
 
 */
@@ -38,16 +38,21 @@ of numbers and record the findings
 #include <stdio.h>
 using namespace std;
 
-void generate_random_list(vector<int> &list, int n);
+void generate_random_list(vector<int>& list, int n);
 void print_list(string desc, vector<int> list);
 void print_result_list(string desc, vector<int> result, clock_t t, FILE* csv_file);
 
-void insertionSort(vector<int> &a);
-void mergeSort(vector<int> &a, int p, int r);
+void insertionSort(vector<int>& a);
+void mergeSort(vector<int>& a, int p, int r);
 void merge(vector<int>& a, int p, int q, int r);
 
-void quickSort(vector<int> &a, int p, int r);
+void quickSort(vector<int>& a, int p, int r);
 int partition(vector<int>& a, int p, int r);
+
+void swap(vector<int>& a, int p, int t);
+int findMedian(vector<int> a, int left, int right);
+int findMedianPosition(vector<int> a, int left, int right);
+void medianSort(vector<int>& a, int left, int right);
 
 int main()
 {
@@ -58,30 +63,13 @@ int main()
     vector<int> result;
     clock_t timeResults[4][10];
     FILE* csv_file = NULL;
-    int eleCounts[5] = { 10, 100, 1000, 10000, 100000 };
-
-    generate_random_list(list, f);
-    //std::sort(list.begin(), list.begin() + list.size());
-    //std::reverse(list.begin(), list.end());
-    list_reserve = list;
-
-    /*std::sort(list.begin(), list.begin() + list.size());
-    std::reverse(list.begin(), list.end());*/
-    //print_list("Before", list);
-    std::printf("Insertion Sort begin...\n");
-    tStart = clock();
-    //mergeSort(list, 0, f - 1);
-    insertionSort(list);
-    tEnd = clock() - tStart;
-    //print_list("After", list);
-    printf(" ended. It took: %dms\n", tEnd);
-    //print_list("After", list_reserve);
-    return 0;
+    const int num_test = 4;
+    int eleCounts[num_test] = { 10, 100, 1000, 10000 };
 
     fopen_s(&csv_file, "C:\\Bob\\sort_bench.csv", "w");
-    std::fprintf(csv_file, "Element Count,Round #,Insertion,Quick,Merge,Built-in\n");
-    
-    for (int z=0; z < 5; z++)
+    std::fprintf(csv_file, "Element Count,Round #,Insertion,Quick,Merge,Median\n");
+
+    for (int z = 0; z < num_test; z++)
     {
         f = eleCounts[z];
 
@@ -89,7 +77,7 @@ int main()
         {
             generate_random_list(list, f);
             std::sort(list.begin(), list.begin() + list.size());
-            //std::reverse(list.begin(), list.end());
+            std::reverse(list.begin(), list.end());
 
             list_reserve = list;
 
@@ -127,9 +115,9 @@ int main()
             // make copy of the org list
             list = list_reserve;
 
-            std::printf("Built-in Sort begin...\n");
+            std::printf("Median Sort begin...\n");
             tStart = clock();
-            std::sort(list.begin(), list.begin() + list.size());
+            medianSort(list, 0, f - 1);
             tEnd = clock() - tStart;
             //print_result_list("Built-in Sort", list, tEnd, csv_file);
             timeResults[3][round] = tEnd;
@@ -137,11 +125,10 @@ int main()
             //std::fprintf(csv_file, "\n");
         }
 
-        
         std::fprintf(csv_file, "Average (%d),_,", f);
         clock_t timeSum = 0;
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < num_test; i++)
         {
             for (int j = 0; j < 10; j++)
             {
@@ -158,7 +145,7 @@ int main()
     return 0;
 }
 
-void insertionSort(vector<int> &a) {
+void insertionSort(vector<int>& a) {
     for (int j = 1; j < a.size(); j++)
     {
         int key = a[j];
@@ -175,7 +162,7 @@ void insertionSort(vector<int> &a) {
 }
 
 // merge sort function
-void mergeSort(vector<int> &a, int p, int r)
+void mergeSort(vector<int>& a, int p, int r)
 {
     int q;
     if (p < r)
@@ -188,7 +175,7 @@ void mergeSort(vector<int> &a, int p, int r)
 }
 
 // function to merge the subarrays
-void merge(vector<int> &a, int p, int q, int r)
+void merge(vector<int>& a, int p, int q, int r)
 {
     vector<int> b(a.size());   //same size of a[]
     int i, j, k;
@@ -246,7 +233,7 @@ void print_result_list(string desc, vector<int> result, clock_t t, FILE* csv_fil
     std::fprintf(csv_file, "%d,", t);
 }
 
-void quickSort(vector<int> &a, int p, int r) {
+void quickSort(vector<int>& a, int p, int r) {
     if (p < r) {
         int q = partition(a, p, r);
         quickSort(a, p, q - 1);
@@ -269,11 +256,86 @@ int partition(vector<int>& a, int p, int r) {
             a[j] = temp;
         }
     }
-    temp = a[i+1];
-    a[i+1] = a[r];
+    temp = a[i + 1];
+    a[i + 1] = a[r];
     a[r] = temp;
 
     return i + 1;
 }
 
+int findMedian(vector<int> a, int left, int right) {
+    return a[findMedianPosition(a, left, right)];
+}
 
+int findMedianPosition(vector<int> a, int left, int right) {
+
+    int size = right - left;
+    int median;
+    int leftCount = (size % 2) == 0 ? size / 2 : (size - 1) / 2;
+    int count = 0;
+
+    for (size_t i = left; i < right + 1; i++)
+    {
+        median = a[i];
+        count = 0;
+        for (size_t j = left + 1; j < right + 1; j++)
+        {
+            if (a[j] < median)
+                count++;
+        }
+
+        if (count == leftCount)
+            return i;
+    }
+
+    return 0;
+}
+
+void swap(vector<int>& a, int p, int t) {
+    int temp;
+    temp = a[p];
+    a[p] = a[t];
+    a[t] = temp;
+}
+
+void medianSort(vector<int>& a, int left, int right) {
+
+    if (left < right) {
+
+        int mid = (left + right) / 2;
+        int medianPos = findMedianPosition(a, left, right);
+        int j = mid;
+
+        swap(a, mid, medianPos);
+
+        for (int i = left; i < mid; i++)
+        {
+            if (a[i] > a[mid]) {
+                for (int k = j + 1; k < a.size(); k++)
+                {
+                    j = k;
+                    if (a[k] <= a[mid]) {
+                        break;
+                    }
+                }
+                swap(a, i, j);
+            }
+        }
+
+        for (j = j + 1; j < right + 1; j++) {
+            if (a[j] <= a[mid]) {
+
+                swap(a, mid, mid + 1);
+
+                if (right - left > 1) {
+                    swap(a, mid, j);
+                }
+
+                mid++;
+            }
+        }
+
+        medianSort(a, left, mid - 1);
+        medianSort(a, mid + 1, right);
+    }
+}
