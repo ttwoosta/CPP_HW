c427d8561c6ab9c44a5b2035f13ec27736037861
Tu Tong <ttwoosta@gmail.com>
2020-02-18 19:34:54 -0500

    Added home work

---
+++ StrNDynamic/StrNDynamic.cpp
@@ -0,0 +1,23 @@
+/*
+1) Implement a string matching function using Finite Automata. 
+The challenge here is in the choice of "compute-transition" function.
+The function should be able to search for any substring within 
+any other longer string and return the index corresponding with 
+the first matching position.
+
+2) We discussed the knapsack problem as an example of Dynamic Programming. 
+Another famous problem is that of the Traveling Salesman. In your own words, 
+describe the traveling salesman problem as well as why this is so hard to solve. 
+Are there any known solutions? Describe how you might go about solving 
+this problem using Dynamic Programming - cite any algorithms or sources 
+that you find in formulating your response.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
